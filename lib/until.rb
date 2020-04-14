fc790b2bc2bd24e4fc48fba54757ea715cc4f74a
Regina Furness <skritschy@gmail.com>
2020-04-14 20:58:17 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,10 @@
-  levitation_force = 6
-
 def using_until
-  until levitation_force>10 do
+    levitation_force = 6
+
+  until levitation_force>10
   levitation_force+=
   puts "Wingardium Leviosa"
   end
+  
 end
 
